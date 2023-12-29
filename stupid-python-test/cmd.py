0f572bf9e53fa3d6ab5deed3d5dbde9202f0f35c
Abraham John <jabrahamjohns@gmail.com>
2023-12-29 14:57:06 +0300

    really this me not knowing at all and truly making the headlines down here

--- stupid-python-test/cmd.py
+++ stupid-python-test/cmd.py
@@ -1,10 +1,7 @@
 import cmd
 
 class HelloWorld(cmd.Cmd):
-    intro = 'what do you say we give it a go'
-    prompt = 'aj'
-    file = None
-
+    
     def do_greetings(self,line):
         print ("Hi AJ here.how are you ?")
 
