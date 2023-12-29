24a791b470e12671446d25d9005bb4c2b217f2fa
Abraham John <jabrahamjohns@gmail.com>
2023-12-29 14:42:45 +0300

    lets check this one out shall we

--- stupid-python-test/cmd.py
+++ stupid-python-test/cmd.py
@@ -4,3 +4,11 @@ class person(cmd.Cmd):
     intro = 'what do you say we give it a go'
     prompt = 'aj'
     file = None
+
+    def greetings(self,line):
+        print "Hi AJ here.how are you ?"
+
+    def EOF(self, line):
+        return True
+if __name__ == '__main__':
+    person().cmdloop
